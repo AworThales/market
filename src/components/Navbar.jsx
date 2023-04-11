19dce304bd4b633b956dfaab9df8a87cf9b0c321
AworThales <thals4jesus@gmail.com>
2023-04-11 10:09:24 -0700

    added navbar contents

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,20 @@ import './navbar.scss';
 const Navbar = () => {
   return (
     <div className='navbar'>
-  
+      <div className="container">
+        <div className="logo">
+          <span className='text'>Thales</span>
+          <span className='dot'>.</span>
+        </div>
+        <div className="links">
+          <span>Thales Business</span>
+          <span>Explore</span>
+          <span>English</span>
+          <span>Sign In</span>
+          <span>Become a Seller</span>
+          <button>Join</button>
+        </div>
+      </div>
     </div>
   )
 }
