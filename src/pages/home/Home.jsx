fad6e6574d4f3f3e13f9ccc0390578be0373127d
AworThales <thals4jesus@gmail.com>
2023-04-13 09:03:21 -0700

    Added trustedBy component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import Featured from '../../components/featured/Featured';
+import TrustedBy from '../../components/trustedBy/TrustedBy';
 import './home.scss';
 
 const Home = () => {
   return (
     <div className='Home'>
       <Featured />
+      <TrustedBy />
     </div>
   )
 }
