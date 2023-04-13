a8472a774a36958b30de566a5d06ddf9abd17c8d
AworThales <thals4jesus@gmail.com>
2023-04-13 06:50:24 -0700

    featured component added

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,14 @@
+import "./featured.scss";
+
+const Featured = () => {
+  return (
+    <div className="featured">
+        <div className="container">
+            
+        </div>
+      
+    </div>
+  )
+}
+
+export default Featured
