8dc2b09529a70268b5921d34df2c82deec10f5dc
AworThales <thals4jesus@gmail.com>
2023-04-13 09:41:01 -0700

    adding bolierplate

--- src/components/trustedBy/TrustedBy.jsx
+++ src/components/trustedBy/TrustedBy.jsx
@@ -1,9 +1,22 @@
 import "./trustedBy.scss";
+import netflix from "../../assets/netflix.png";
+import paypal from "../../assets/paypal.png";
+import google from "../../assets/google.png";
+import facebook from "../../assets/facebook.png";
+import pg from "../../assets/pg.png";
+
 
 const TrustedBy = () => {
   return (
     <div className="trustedBy">
-      
+      <div className="container">
+      <span>Trusted by:</span>
+      <img src={netflix} alt="" />
+      <img src={paypal} alt="" />
+      <img src={google} alt="" />
+      <img src={facebook} alt="" />
+      <img src={pg} alt="" />
+      </div>
     </div>
   )
 }
