5c0c195d85770e85403397b5eb4c4da92705874c
AworThales <thals4jesus@gmail.com>
2023-04-14 09:41:03 -0700

    Maping my cards as the children

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,13 +2,20 @@ import Featured from '../../components/featured/Featured';
 import Slide from '../../components/slide/Slide';
 import TrustedBy from '../../components/trustedBy/TrustedBy';
 import './home.scss';
+import {cards} from "../../data";
+import CatCard from '../../components/catCard/CatCard';
 
 const Home = () => {
   return (
     <div className='Home'>
       <Featured />
       <TrustedBy />
-      <Slide />
+      <Slide slidesToShow={5} arrowsScroll={5}>
+        {/* mapping my cards as the children */}
+      {cards.map((card) => (
+              <CatCard item={card} key={card.id} />
+              ))}
+      </Slide>
     </div>
   )
 }
