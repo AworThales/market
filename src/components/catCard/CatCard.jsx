2396b21f210cc096d4ab941d4de3da331270264a
AworThales <thals4jesus@gmail.com>
2023-04-16 10:46:10 -0700

    Items not updating, pass this on the comp. func so it can update. and then delete text on desc and tile span element

--- src/components/catCard/CatCard.jsx
+++ src/components/catCard/CatCard.jsx
@@ -1,13 +1,13 @@
 import { Link } from "react-router-dom";
 import "./catCard.scss";
 
-const CatCard = (item) => {
+const CatCard = ({item}) => {
   return (
     <Link to="/gigs?cat=design">
         <div className="catCard">
             <img src={item.img} alt="" />
-            <span className="desc">{item.desc}Description Here</span>
-            <span className="title">{item.title}Title Here</span>
+            <span className="desc">{item.desc}</span>
+            <span className="title">{item.title}</span>
         </div>
     </Link>
   )
