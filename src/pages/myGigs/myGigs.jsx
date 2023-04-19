c4b567b5cc479be7768e4a86760ba8c42f6636bb
AworThales <thals4jesus@gmail.com>
2023-04-19 09:44:40 -0700

    myGig components  boilerplate updated

--- src/pages/myGigs/myGigs.jsx
+++ src/pages/myGigs/myGigs.jsx
@@ -1,11 +1,84 @@
-import './mygigs.scss';
+import { Link } from "react-router-dom";
+import "./myGigs.scss";
 
-const myGigs = () => {
+const MyGigs = () => {
   return (
-    <div className='myGigs'>
-      myGigs
+    <div className="myGigs">
+      <div className="container">
+        <div className="title">
+          <h1>Gigs</h1>
+          <Link to="/add">
+            <button>Add New Gig</button>
+          </Link>
+        </div>
+        <table>
+          <tr>
+            <th>Image</th>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Sales</th>
+            <th>Action</th>
+          </tr>
+          <tr>
+            <td>
+              <img className="img" src="https://images.pexels.com/photos/11295165/pexels-photo-11295165.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" />
+            </td>
+            <td>Gig1</td>
+            <td>100</td>
+            <td>150</td>
+            <td>
+              <img className="delete" src="/img/delete.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img className="img" src="https://images.pexels.com/photos/11295165/pexels-photo-11295165.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" />
+            </td>
+            <td>Gig1</td>
+            <td>100</td>
+            <td>150</td>
+            <td>
+              <img className="delete" src="/img/delete.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img className="img" src="https://images.pexels.com/photos/11295165/pexels-photo-11295165.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" />
+            </td>
+            <td>Gig1</td>
+            <td>100</td>
+            <td>150</td>
+            <td>
+              <img className="delete" src="/img/delete.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img className="img" src="https://images.pexels.com/photos/11295165/pexels-photo-11295165.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" />
+            </td>
+            <td>Gig1</td>
+            <td>100</td>
+            <td>150</td>
+            <td>
+              <img className="delete" src="/img/delete.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img className="img" src="https://images.pexels.com/photos/11295165/pexels-photo-11295165.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" />
+            </td>
+            <td>Gig1</td>
+            <td>100</td>
+            <td>150</td>
+            <td>
+              <img className="delete" src="/img/delete.png" alt="" />
+            </td>
+          </tr>
+        </table>
+      </div>
+      
     </div>
   )
 }
 
-export default myGigs
+export default MyGigs
