f11fe5794012da75c43d7bc07b048ab73ed56d50
AworThales <thals4jesus@gmail.com>
2023-04-19 09:48:05 -0700

    Updated boilerplate and contents of Orders component

--- src/pages/orders/Orders.jsx
+++ src/pages/orders/Orders.jsx
@@ -1,9 +1,86 @@
+import { Link } from 'react-router-dom';
 import './orders.scss';
 
 const Orders = () => {
+
+  const currentUser = {
+    id: 1,
+    username: "Thales Solo",
+    isSeller: true
+  }
+
   return (
-    <div className='order'>
-      order
+    <div className='orders'>
+      <div className="container">
+      <div className="title">
+          <h1>Orders</h1>
+        </div>
+      <table>
+          <tr>
+            <th>Image</th>
+            <th>Title</th>
+            <th>Price</th>
+            {/* is current user a seller? then buyer otherwise seller */}
+            <th>{currentUser?.isSeller ? "Buyer" : "Seller"}</th>
+            <th>Contact</th>
+          </tr>
+          <tr>
+            <td>
+              <img className="img" src="https://images.pexels.com/photos/11295165/pexels-photo-11295165.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" />
+            </td>
+            <td>Gig1</td>
+            <td>100</td>
+            <td>150</td>
+            <td>
+              <img className="delete" src="/img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img className="img" src="https://images.pexels.com/photos/11295165/pexels-photo-11295165.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" />
+            </td>
+            <td>Gig1</td>
+            <td>100</td>
+            <td>150</td>
+            <td>
+              <img className="delete" src="/img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img className="img" src="https://images.pexels.com/photos/11295165/pexels-photo-11295165.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" />
+            </td>
+            <td>Gig1</td>
+            <td>100</td>
+            <td>150</td>
+            <td>
+              <img className="delete" src="/img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img className="img" src="https://images.pexels.com/photos/11295165/pexels-photo-11295165.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" />
+            </td>
+            <td>Gig1</td>
+            <td>100</td>
+            <td>150</td>
+            <td>
+              <img className="delete" src="/img/message.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img className="img" src="https://images.pexels.com/photos/11295165/pexels-photo-11295165.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" />
+            </td>
+            <td>Gig1</td>
+            <td>100</td>
+            <td>150</td>
+            <td>
+              <img className="delete" src="/img/message.png" alt="" />
+            </td>
+          </tr>
+        </table>
+      </div>
     </div>
   )
 }
