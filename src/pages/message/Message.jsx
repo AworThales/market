edd78387ea356a2572ae9287c4c8b99b955405d4
AworThales <thals4jesus@gmail.com>
2023-04-20 06:11:56 -0700

    Message page component updated and added functionality

--- src/pages/message/Message.jsx
+++ src/pages/message/Message.jsx
@@ -1,9 +1,95 @@
+import { Link } from 'react-router-dom';
 import './message.scss';
 
 const Message = () => {
   return (
     <div className='message'> 
-      message
+      <div className="container">
+        <span className="breadcrumbs">
+          <Link to="/messages" className='link'>MESSAGES</Link> <b> > THALES SOLO > </b>
+        </span>
+        <div className="messages">
+          <div className="item">
+            <img src="https://images.pexels.com/photos/7532110/pexels-photo-7532110.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Lorem ipsum dolor sit amet consectetur adipisicing.
+              Lorem ipsum dolor sit amet consectetur.
+            </p>
+          </div>
+          <div className="item owner">
+            <img src="https://images.pexels.com/photos/4371669/pexels-photo-4371669.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Lorem ipsum dolor sit amet consectetur adipisicing.
+              Lorem ipsum dolor sit amet consectetur.
+            </p>
+          </div>
+          <div className="item">
+            <img src="https://images.pexels.com/photos/7532110/pexels-photo-7532110.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Lorem ipsum dolor sit amet consectetur adipisicing.
+              Lorem ipsum dolor sit amet consectetur.
+            </p>
+          </div>
+          <div className="item owner">
+            <img src="https://images.pexels.com/photos/4371669/pexels-photo-4371669.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Lorem ipsum dolor sit amet consectetur adipisicing.
+              Lorem ipsum dolor sit amet consectetur.
+            </p>
+          </div>
+          <div className="item">
+            <img src="https://images.pexels.com/photos/7532110/pexels-photo-7532110.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Lorem ipsum dolor sit amet consectetur adipisicing.
+              Lorem ipsum dolor sit amet consectetur.
+            </p>
+          </div>
+          <div className="item owner">
+            <img src="https://images.pexels.com/photos/4371669/pexels-photo-4371669.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Lorem ipsum dolor sit amet consectetur adipisicing.
+              Lorem ipsum dolor sit amet consectetur.
+            </p>
+          </div>
+          <div className="item">
+            <img src="https://images.pexels.com/photos/7532110/pexels-photo-7532110.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Lorem ipsum dolor sit amet consectetur adipisicing.
+              Lorem ipsum dolor sit amet consectetur.
+            </p>
+          </div>
+          <div className="item owner">
+            <img src="https://images.pexels.com/photos/4371669/pexels-photo-4371669.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Lorem ipsum dolor sit amet consectetur adipisicing.
+              Lorem ipsum dolor sit amet consectetur.
+            </p>
+          </div>
+        </div>
+
+        <hr />
+
+        <div className="write">
+          <textarea name="" placeholder='Write a message...' id="" cols="30" rows="10"></textarea>
+          <button>Send</button>
+        </div>
+      </div>
     </div>
   )
 }
