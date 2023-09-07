50bf362991178fd19ebe40c092181ecae95065bc
AworThales <thals4jesus@gmail.com>
2023-09-07 13:14:48 +0100

    Message Schema update

--- backend/models/message.model.js
+++ backend/models/message.model.js
@@ -2,7 +2,19 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const MessageSchema = new Schema({
-   
+    consersationId: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    userId: {
+        type: String,
+        required: true,
+    },
+    desc: {
+        type: String,
+        required: true,
+    },
 },{timestamps:true});
 
 export default mongoose.model("Message",  MessageSchema);
