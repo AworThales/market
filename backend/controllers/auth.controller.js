88ad7b3bdd808122fac06ea084e348c47f289cb9
AworThales <thals4jesus@gmail.com>
2023-09-07 18:38:14 +0100

    passing error handle function

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,8 +1,9 @@
 import UserModel from "../models/user.model.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import createError from "../utils/createError.js";
 
-export const register = async (req, res) =>{
+export const register = async (req, res, next) =>{
     try{
         const hashPassword = bcrypt.hashSync(req.body.password, 10);
         const newUser = new UserModel({
@@ -13,16 +14,16 @@ export const register = async (req, res) =>{
         return res.status(200).send("User created successfully!");
     }
     catch(err){
-        return res.status(500).send("Something went wrong!");
+        next(err);
     }
 };
 
-export const login = async (req, res) =>{
+export const login = async (req, res, next) =>{
     try {
         const user = await UserModel.findOne({username: req.body.username});
-        if(!user) return res.status(404).send("User not found!");
+        if(!user) return next(createError(404, "User was not found!"));
         const correctPassword = await bcrypt.compareSync(req.body.password, user.password);
-        if(!correctPassword) return res.status(404).send("Wrong username or Password!");
+        if(!correctPassword) return next(createError(404, "Wrong username or password!"));
 
         const token = jwt.sign({
             id: user._id,
@@ -31,14 +32,18 @@ export const login = async (req, res) =>{
         );
 
         const { password, ...theRestInfo} = user._doc;
-        return res.cookie("accessToken", token, {
+        res.cookie("access_Token", token, {
             httpOnly: true,
         }).status(200).send(theRestInfo);
     } catch (err) {
-        return res.status(500).send("Something went wrong!");
+        next(err);
     }
 };
 
 export const logout = async (req, res) =>{
-
+    res.clearCookie("accessToken", {
+        sameSite: "none",
+        secure: true,
+    })
+    .status(200).send("User logged out successfully!");
 };
