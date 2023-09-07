060fe86370facc732247685108aa9e3d744c297c
AworThales <thals4jesus@gmail.com>
2023-09-07 14:01:00 +0100

    Importing Routes

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoute from "./routes/user.route.js";
+import gigRoute from "./routes/gig.route.js";
+import conversationRoute from "./routes/conversation.route.js";
+import messageRoute from "./routes/message.route.js";
+import orderRoute from "./routes/order.route.js";
+import reviewRoute from "./routes/review.route.js";
 
 
 const app = express();
 dotenv.config();
-
 
 const connect = async ()=>{
     try {
@@ -15,6 +20,13 @@ const connect = async ()=>{
         console.log(error);
     }
 };
+
+app.use("/api/users", userRoute);
+app.use("/api/gigs", gigRoute);
+app.use("/api/conversation", conversationRoute);
+app.use("/api/messages", messageRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/reviews", reviewRoute);
 
 app.listen(process.env.PORT || 5000, () =>{
     connect();
