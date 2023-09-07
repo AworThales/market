e5c210344ccd572a8d23d4b00ad82fbbe1c1c76c
AworThales <thals4jesus@gmail.com>
2023-09-07 13:10:58 +0100

    user schema update

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -2,7 +2,40 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
-
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    img: {
+        type: String,
+        required: false,
+    },
+    country: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: String,
+        required: false,  //we are to ask phone num for only sellers
+    },
+    desc: {
+        type: String,
+        required: false,
+    },
+    isSeller: {
+        type: Boolean,
+        required: false,
+    },
 },{timestamps:true});
 
 export default mongoose.model("User",  UserSchema);
