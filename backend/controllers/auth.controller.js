d1e4a1de74f3fe3189c1b366b3c0562b7c4c7081
AworThales <thals4jesus@gmail.com>
2023-09-07 14:12:31 +0100

    Initialize file

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,3 +1,11 @@
-export const fn = async (req, res) =>{
+export const register = async (req, res) =>{
+
+};
+
+export const login = async (req, res) =>{
+
+};
+
+export const logout = async (req, res) =>{
 
 };
