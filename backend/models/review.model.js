9f2180ef0e7c8f131412144b374d0183aad013fc
AworThales <thals4jesus@gmail.com>
2023-09-07 13:12:19 +0100

    Review Schema update

--- backend/models/review.model.js
+++ backend/models/review.model.js
@@ -2,7 +2,23 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const ReviewSchema = new Schema({
-    
+    gigId: {
+        type: String,
+        required: true,
+    },
+    userId: {
+        type: String,
+        required: true,
+    },
+    desc: {
+        type: String,
+        required: true,
+    },
+    star: {
+        type: Number,
+        required: 0,
+        enum: [1,2,3,4,5],   //stars can only be in this number range
+    },
 },{timestamps:true});
 
 export default mongoose.model("Review",  ReviewSchema);
