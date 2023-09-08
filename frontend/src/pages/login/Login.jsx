cdd1ffd174ead6d29d98bf86d7d9443bb0270ae7
AworThales <thals4jesus@gmail.com>
2023-09-08 18:11:23 +0100

    Login page done

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,9 +1,48 @@
 import './login.scss';
+import { useNavigate } from "react-router-dom";
+import newRequest from "../../utils/newRequest";
+import { useState } from 'react';
 
 const Login = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
+
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+        const res = await newRequest.post("/auth/login/", { username, password });
+        console.log(res);
+        localStorage.setItem("currentUser", JSON.stringify(res.data));
+        navigate("/")
+        } catch (err) {
+        setError(err.response.data);
+        }
+    };
+
   return (
     <div className='login'>
-      Login
+        <form onSubmit={handleSubmit}>
+            <h1>Sign in</h1>
+            <label htmlFor="">Username</label>
+            <input
+            name="username"
+            type="text"
+            placeholder="thales"
+            onChange={(e) => setUsername(e.target.value)}
+            />
+
+            <label htmlFor="">Password</label>
+            <input
+            name="password"
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+            />
+            <button type="submit">Login</button>
+            {error && error}
+      </form>
     </div>
   )
 }
