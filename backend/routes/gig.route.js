a9cd424c36d89405608c574a00bac7129d1e0346
AworThales <thals4jesus@gmail.com>
2023-09-10 11:50:32 +0100

    Git route

--- backend/routes/gig.route.js
+++ backend/routes/gig.route.js
@@ -1,7 +1,12 @@
 import express from "express";
+import {verifyToken} from "../middleware/verify.js"
+import { createGig, deleteGig, getGig, getGigs } from "../controllers/gig.controller.js";
 
 const router = express.Router();
 
-router.get("/");
+router.post("/", verifyToken, createGig)
+router.delete("/:id", verifyToken, deleteGig );
+router.get("/single/:id", verifyToken, getGig )
+router.get("/", verifyToken, getGigs);
 
 export default router;
