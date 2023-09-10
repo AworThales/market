3c90e5306507973230bd58f0beaeaf0b139721b2
AworThales <thals4jesus@gmail.com>
2023-09-10 13:50:26 +0100

    Updated with api data

--- frontend/src/pages/gigs/Gigs.jsx
+++ frontend/src/pages/gigs/Gigs.jsx
@@ -1,30 +1,60 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './gigs.scss';
 import GigCard from '../../components/gigCard/GigCard';
 import {gigs} from "../../data";
+import { useQuery } from '@tanstack/react-query';
+import newRequest from '../../utils/newRequest';
+import { useLocation } from 'react-router-dom';
 
 
 const Gigs = () => {
   const [sort, setSort] = useState("sales");
   const [open, setOpen] = useState(false);
+  const minRef = useRef();
+  const maxRef = useRef();
+  const { search } = useLocation();
+  console.log(search)
 
+  /** Fetching data with react query */
+  const { isLoading, error, refetch, data } = useQuery({
+    queryKey: ['gigs'],
+    queryFn: () =>
+      newRequest.get(`/gigs${search}&min=${minRef.current.value}&max=${maxRef.current.value}&sort=${sort}`)
+      .then((res)=>{
+        return res.data;
+      })
+  });
+  console.log(data)
+
+
+  /** resorting by type */
   const reSort = (type) => {
     setSort(type)
     setOpen(false)
   }
 
+  /** Refetching sort again in its changes */
+  useEffect(()=>{
+    refetch();
+  },[sort]);
+
+  /** Refetching min and max search */
+  const apply = () => {
+   refetch();
+  };
+
   return (
     <div className='gigs'>
       <div className="container">
-        <span className="breadcrumbs">THALES > GRAPHICS & DESIGN</span>
+        <span className="breadcrumbs">THALES > GRAPHICS & DESIGN > </span>
         <h1>AI Artists</h1>
         <p>Explore the boundaries of art and technology with Thales's AI artists</p>
         <div className="menu">
           <div className="left">
             <span>Budget</span>
-            <input type="text" placeholder='min' />
-            <input type="text" placeholder='max' />
-            <button>Apply</button>
+            <input ref={minRef} type="text" placeholder='min' />
+            <input ref={maxRef} type="text" placeholder='max' />
+            <button onClick={apply}>Apply</button>
           </div>
           <div className="right">
             <span className="sortBy">SortBy</span>
@@ -32,19 +62,26 @@ const Gigs = () => {
             {/* giving a condition to check if down arrow icon is click it use reverse to opposite of open useState. */}
             <img src="./img/down.png" alt="" onClick={() => setOpen(!open)} />
             {/* if the open useState exists the display the rightmenu class */}
-            {open && (<div className="rightMenu">
-              {sort === "sales" ? (<span onClick={() =>reSort("createsAt")}>Newest</span> 
-              ) : ( 
-              <span onClick={() =>reSort("sales")}>Best Selling</span>
-              )}
-             </div>
+            {open && (
+              <div className="rightMenu">
+                {sort === "sales" ? (
+                  <span onClick={() => reSort("createdAt")}>Newest</span>
+                ) : (
+                  <span onClick={() => reSort("sales")}>Best Selling</span>
+                )}
+                <span onClick={() => reSort("sales")}>Popular</span>
+              </div>
             )}
           </div>
         </div>
         <div className="cards">
-          {gigs.map((gig) => (
-            <GigCard item={gig} key={gig.id} />
+          {isLoading
+            ? "Loading..." 
+            : error 
+            ? "OOPS.. Something is wrong!"
+            : data.map((gig) => (<GigCard item={gig} key={gig._id} />
           ))}
+        
         </div>
       </div>
     </div>
