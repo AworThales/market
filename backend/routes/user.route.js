031a96347ab7e8d7f85e293fd7fb24d84e89fde2
AworThales <thals4jesus@gmail.com>
2023-09-10 13:52:16 +0100

    user route updated

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { deleteUser } from "../controllers/user.controller.js";
+import { deleteUser, getUser } from "../controllers/user.controller.js";
 import { verifyToken } from "../middleware/verify.js";
 
 const router = express.Router();
 
-router.delete("/:userId", verifyToken, deleteUser);
+router.delete("/:id", verifyToken, deleteUser);
+router.get("/:id", verifyToken, getUser);
 
 export default router;
