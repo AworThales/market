bbef063af0e0a06c3a45105c22e714f07d534adf
AworThales <thals4jesus@gmail.com>
2023-09-11 14:59:31 +0100

    Review route updated

--- backend/routes/review.route.js
+++ backend/routes/review.route.js
@@ -1,7 +1,11 @@
 import express from "express";
+import { verifyToken } from "../middleware/verify.js";
+import { createReview, deleteReview, getReview } from "../controllers/review.controller.js";
 
 const router = express.Router();
 
-router.get("/");
+router.post("/", verifyToken, createReview);
+router.get("/:gigId", verifyToken, getReview);
+router.get("/:id", verifyToken, deleteReview);
 
 export default router;
