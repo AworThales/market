4ca142aa09ef73863bf882065c3b703952e6dcfc
AworThales <thals4jesus@gmail.com>
2023-09-11 14:57:41 +0100

    Gig file updated with api data

--- frontend/src/pages/gig/Gig.jsx
+++ frontend/src/pages/gig/Gig.jsx
@@ -1,61 +1,102 @@
 import { Slider } from 'infinite-react-carousel/lib';
 import './gig.scss';
+import newRequest from '../../utils/newRequest';
+import { useQuery } from '@tanstack/react-query';
+import { useParams } from 'react-router-dom';
+import { Reviews } from '../../components/reviews/Reviews';
 
 
 const Gig = () => {
+  const {id} = useParams();
+  console.log(id);
+  /** Fetching gig data with react query */
+  const { isLoading, error, data } = useQuery({
+    queryKey: ['gig'],
+    queryFn: () =>
+      newRequest.get(`/gigs/single/${id}`)
+      .then((res)=>{
+        return res.data;
+      })
+  });
+
+  const userId = data?.userId;
+
+  /** Fetching user data with react query */
+  const { 
+    isLoading: isLoadingUser,
+     error: errorUser, 
+     data: dataUser,
+    } = useQuery({
+    queryKey: ['user'],
+    queryFn: () =>
+      newRequest.get(`/users/${userId}`)
+      .then((res)=>{
+        return res.data;
+      }),
+      enabled: !!userId,
+  });
+  // console.log(dataUser)
+  // console.log(!isLoading && Array (Math.round(data.totalStars / data.starNumber)).fill())
+
   return (
     <div className='gig'>
-      <div className="container">
+      { isLoading ? "Loading..." : error ? "Oops! Something is wrong!" :
+       <div className="container">
         <div className="left">
-          <span className="breadCrumbs">THALES > GRAPHICS & DESIGN</span>
-          <h1>I will create ai generated art for you</h1>
-          <div className="user">
-            <img className='img' src="https://images.pexels.com/photos/1771383/pexels-photo-1771383.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
-            <span>Thales Solo</span>
-            <div className="stars">
-              <img src="/img/star.png" alt="" />
-              <img src="/img/star.png" alt="" />
-              <img src="/img/star.png" alt="" />
-              <img src="/img/star.png" alt="" />
-              <img src="/img/star.png" alt="" />
-              <span>5</span>
-            </div>
-          </div>
+          <span className="breadCrumbs">THALES {">"} GRAPHICS & DESIGN {">"}</span>
+          <h1 className='gigTitle'>{data.title}</h1>
+          {isLoadingUser ? (
+            "Loading..."
+            ) : errorUser ? (
+              "Ooops! Something is wrong!"
+            ) : (
+            <div className="user">
+              <img className={dataUser.img || "/img/noavatar.jpg"} alt="" />
+              <span>{dataUser.username}</span>
+              {!isNaN(data.totalStars / data.starNumber) && (
+              <div className="stars">
+                {Array (Math.round(data.totalStars / data.starNumber))
+                  .fill()
+                  .map((item,i)=>(
+                    <img src="/img/star.png" alt="" key={i}/>
+                ))}
+                <span className='gigstars'>{Math.round(data.totalStars / data.starNumber)}</span>
+              </div>
+              )}
+          </div>)}
           {/* slider carousel */}
           <Slider slideToShow={1} arrowScroll={1} className="slider">
-            <img src="https://images.pexels.com/photos/8797307/pexels-photo-8797307.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
-            <img src="https://images.pexels.com/photos/5490778/pexels-photo-5490778.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
-            <img src="https://images.pexels.com/photos/8100784/pexels-photo-8100784.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+            {data.images.map((img)=>(
+            <img src={img} alt="" key={img} />
+            ))}
+            
           </Slider>
           <h2>About This Gig</h2>
-          <p>
-            i use an AI program to create images based on text prompts. This
-            means i can help you to create a vision you have through a textual
-            description of your scene without requiring any reference images.
-            Some things I've found it often excels at are: Character portraits
-            (E.g. a picture to go with your DnD charactr) Landscapes (E.g. team,
-            business, profile picture). You can be as vague or as descriptive as
-            you want. Being more vague will allow the AI to be more creative which
-            can sometimes result in some amazing images. You can also be credibly 
-            precise if you have a clear image of what you want in mind. All of the 
-            images i can create are original and will be found nowhere else. If you
-            have any questions you're more than welcome to send me a message.
-          </p>
+          <p className='gigDesc'>{data.desc}</p>
           {/* seller section */}
+          {isLoadingUser ? (
+            "Loading..."
+            ) : errorUser ? (
+              "Ooops! Something is wrong!"
+            ) : (
           <div className="seller">
             <h2>About The Seller</h2>
             <div className="user">
-              <img src="https://images.pexels.com/photos/1771383/pexels-photo-1771383.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+              <img src={dataUser.img || "/img/noavatar.jpg"} alt="" />
               <div className="info">
-                <span>Thales Solo</span>
-                <div className="stars">
-                  <img src="/img/star.png" alt="" />
-                  <img src="/img/star.png" alt="" />
-                  <img src="/img/star.png" alt="" />
-                  <img src="/img/star.png" alt="" />
-                  <img src="/img/star.png" alt="" />
-                  <span>5</span>
-                </div>
+                <span className='username'>{dataUser.username}</span>
+                {!isNaN(data.totalStars / data.starNumber) && (
+            <div className="stars">
+              {Array (Math.round(data.totalStars / data.starNumber))
+                .fill()
+                .map((item,i)=>(
+                  <img src="/img/star.png" alt="" key={i}/>
+              ))}
+              <span className='gigstars'>
+                  {Math.round(data.totalStars / data.starNumber)}
+              </span>
+            </div>
+            )}
                 <button>Contact Me</button>
               </div>
             </div>
@@ -63,7 +104,7 @@ const Gig = () => {
               <div className="items">
                 <div className="item">
                   <span className="title">From</span>
-                  <span className="desc">Akamkpa</span>
+                  <span className="desc">{dataUser.country}</span>
                 </div>
                 <div className="item">
                   <span className="title">Member since</span>
@@ -86,171 +127,41 @@ const Gig = () => {
               <hr />
 
               <p>
-                My Name is Thales, i enjoy creating AI generated art in 
-                my spare time. i have a lot of experience using the AI
-                program and that means i know what to prompt the AI with
-                to get a great and incredibly detailed result.
+                {data.desc}
               </p>
             </div>
-          </div>
-          <div className="reviews">
-            <h2>Reviews</h2>
-            <div className="item">
-              <div className="user">
-                <img className='img' src="https://images.pexels.com/photos/1771383/pexels-photo-1771383.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
-                <div className="info">
-                  <span>Thales Solo</span>
-                  <div className="country">
-                    <img src="https://images.pexels.com/photos/8797307/pexels-photo-8797307.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
-                    <span>Nigeria</span>
-                  </div>
-                </div>
-              </div>
-              <div className="stars">
-                  <img src="/img/star.png" alt="" />
-                  <img src="/img/star.png" alt="" />
-                  <img src="/img/star.png" alt="" />
-                  <img src="/img/star.png" alt="" />
-                  <img src="/img/star.png" alt="" />
-                  <span>5</span>
-                </div>
-                <p>
-                  i Just want to say that art_with_ai was the first, and after
-                  this, the only artist Ill be using on Thales. Communication
-                  was amazing, each and every day he sent me images that i was 
-                  free to request changes to. They listened, understood, and 
-                  delivered above and beyond my expectations. I absolutely 
-                  recommend this gig, and know already that be using it again
-                  very soon.
-                </p>
-                <div className="helpful">
-                  <span>Helpful?</span>
-                  <img src="/img/like.png" alt="" />
-                  <span>Yes</span>
-                  <img src="/img/dislike.png" alt="" />
-                  <span>No</span>
-                </div>
-            </div>
+          </div>)}
 
-            <hr />
-            <div className="item">
-              <div className="user">
-                <img className='img' src="https://images.pexels.com/photos/1771383/pexels-photo-1771383.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
-                <div className="info">
-                  <span>Thales Awor</span>
-                  <div className="country">
-                    <img src="https://images.pexels.com/photos/8797307/pexels-photo-8797307.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
-                    <span>South Africa</span>
-                  </div>
-                </div>
-              </div>
-              <div className="stars">
-                  <img src="/img/star.png" alt="" />
-                  <img src="/img/star.png" alt="" />
-                  <img src="/img/star.png" alt="" />
-                  <img src="/img/star.png" alt="" />
-                  <img src="/img/star.png" alt="" />
-                  <span>5</span>
-                </div>
-                <p>
-                  i Just want to say that art_with_ai was the first, and after
-                  this, the only artist Ill be using on Thales. Communication
-                  was amazing, each and every day he sent me images that i was 
-                  free to request changes to. They listened, understood, and 
-                  delivered above and beyond my expectations. I absolutely 
-                  recommend this gig, and know already that be using it again
-                  very soon.
-                </p>
-                <div className="helpful">
-                  <span>Helpful?</span>
-                  <img src="/img/like.png" alt="" />
-                  <span>Yes</span>
-                  <img src="/img/dislike.png" alt="" />
-                  <span>No</span>
-                </div>
-            </div>
-
-            <hr />
-
-            <div className="item">
-              <div className="user">
-                <img className='img' src="https://images.pexels.com/photos/1771383/pexels-photo-1771383.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
-                <div className="info">
-                  <span>Thales Solo</span>
-                  <div className="country">
-                    <img src="https://images.pexels.com/photos/8797307/pexels-photo-8797307.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
-                    <span>Nigeria</span>
-                  </div>
-                </div>
-              </div>
-              <div className="stars">
-                  <img src="/img/star.png" alt="" />
-                  <img src="/img/star.png" alt="" />
-                  <img src="/img/star.png" alt="" />
-                  <img src="/img/star.png" alt="" />
-                  <img src="/img/star.png" alt="" />
-                  <span>5</span>
-                </div>
-                <p>
-                  i Just want to say that art_with_ai was the first, and after
-                  this, the only artist Ill be using on Thales. Communication
-                  was amazing, each and every day he sent me images that i was 
-                  free to request changes to. They listened, understood, and 
-                  delivered above and beyond my expectations. I absolutely 
-                  recommend this gig, and know already that be using it again
-                  very soon.
-                </p>
-                <div className="helpful">
-                  <span>Helpful?</span>
-                  <img src="/img/like.png" alt="" />
-                  <span>Yes</span>
-                  <img src="/img/dislike.png" alt="" />
-                  <span>No</span>
-                </div>
-            </div>
-
-          </div>
+          <Reviews gigId={id}/>
         </div>
         <div className="right">
           <div className="price">
-            <h3>1 AI generated image</h3>
-            <h2>$ 60.85</h2>
+            <h3 className='shortTitle'>{data.shortTitle}</h3>
+            <h2>$ {data.price}</h2>
           </div>
-          <p>
-            I will create a unique high AI generated image based
-            on a description that you give me.
-          </p>
+          <p className='shortDesc'>{data.shortDesc}</p>
           <div className="details">
             <div className="item">
               <img src="/img/clock.png" alt="" />
-              <span>2 days Delivery</span>
+              <span className='deliveryTime'>{data.deliveryTime} days Delivery</span>
             </div>
             <div className="item">
               <img src="/img/recycle.png" alt="" />
-              <span>4 Revisions</span>
+              <span className='revisionNumber'>{data.revisionNumber} Revisions</span>
             </div>
           </div>
           <div className="features">
-            <div className="item">
+            {data.features.map((feature)=>(
+            <div className="item" key={feature}>
               <img src="/img/greencheck.png" alt="" />
-              <span>Promt writing</span>
+              <span>{feature}</span>
             </div>
-            <div className="item">
-              <img src="/img/greencheck.png" alt="" />
-              <span>Promt writing</span>
-            </div>
-            <div className="item">
-              <img src="/img/greencheck.png" alt="" />
-              <span>Promt writing</span>
-            </div>
-            <div className="item">
-              <img src="/img/greencheck.png" alt="" />
-              <span>Promt writing</span>
-            </div>
+            ))}
           </div>
           <button>Continue</button>
         </div>
       </div>
+      }
     </div>
   )
 }
