a69a1a3746b93278ad9ad5d8999134bf7f260afd
AworThales <thals4jesus@gmail.com>
2023-09-14 11:40:52 +0100

    message controller updated

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,3 +1,36 @@
-export const fn = async (req, res) =>{
+import MessageModel from "../models/message.model.js";
+import ConversationModel from "../models/conversation.model.js";
 
+export const createMessage = async (req, res, next) => {
+  const newMessage = new Message({
+    conversationId: req.body.conversationId,
+    userId: req.userId,
+    desc: req.body.desc,
+  });
+  try {
+    const savedMessage = await newMessage.save();
+    await ConversationModel.findOneAndUpdate(
+      { id: req.body.conversationId },
+      {
+        $set: {
+          readBySeller: req.isSeller,
+          readByBuyer: !req.isSeller,
+          lastMessage: req.body.desc,
+        },
+      },
+      { new: true }
+    );
+
+    res.status(201).send(savedMessage);
+  } catch (err) {
+    next(err);
+  }
+};
+export const getMessages = async (req, res, next) => {
+  try {
+    const messages = await MessageModel.find({ conversationId: req.params.id });
+    res.status(200).send(messages);
+  } catch (err) {
+    next(err);
+  }
 };
