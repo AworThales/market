8ca869a485688b26223da24ee60e98ae2be42d5a
AworThales <thals4jesus@gmail.com>
2023-09-14 11:36:47 +0100

    updating order route

--- backend/routes/order.route.js
+++ backend/routes/order.route.js
@@ -1,7 +1,10 @@
 import express from "express";
+import { createOrder, getOrders } from "../controllers/order.controller.js";
+import {verifyToken} from "../middleware/verify.js"
 
 const router = express.Router();
 
-router.get("/");
+router.post("/:gigId", verifyToken, createOrder);
+router.get("/", verifyToken, getOrders);
 
 export default router;
