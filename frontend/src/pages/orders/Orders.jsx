b06eb00b105862a0c2d91b266cc2aaaecfae29c1
AworThales <thals4jesus@gmail.com>
2023-09-14 11:34:11 +0100

    Order page updated with api data

--- frontend/src/pages/orders/Orders.jsx
+++ frontend/src/pages/orders/Orders.jsx
@@ -1,86 +1,73 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './orders.scss';
+import newRequest from '../../utils/newRequest';
+import { useQuery } from "@tanstack/react-query";
 
 const Orders = () => {
+  const navigate = useNavigate();
+  const currentUser = JSON.parse(localStorage.getItem("currentUser"));
 
-  const currentUser = {
-    id: 1,
-    username: "Thales Solo",
-    isSeller: true
-  }
+   /** Fetching orders data with react query */
+   const { isLoading, error, data } = useQuery({
+    queryKey: ['orders'],
+    queryFn: () =>
+      newRequest.get("/orders")
+      .then((res)=>{
+        return res.data;
+      })
+  });
+
+  const handleContact = async (order)=>{
+    const sellerId = order.sellerId;
+    const buyerId = order.buyerId;
+    const id = sellerId + buyerId;
+   
+    try {
+      const res = await newRequest.get(`/conversations/single/${id}`);
+      navigate(`/message/${res.data.id}`)
+    } catch (err) {
+      if(err.response.status === 404){
+        const res = await newRequest.get(`/conversations`,
+          {to: currentUser.isSeller ? buyerId : sellerId }
+        );
+        navigate(`/message/${res.data.id}`);
+      }
+    }
+  };
 
   return (
     <div className='orders'>
+      {isLoading 
+      ? ("Loading...") 
+      : error 
+      ? ("Ooops! Something is wrong" )
+      : (
       <div className="container">
-      <div className="title">
+        <div className="title">
           <h1>Orders</h1>
         </div>
-      <table>
-          <tr>
-            <th>Image</th>
-            <th>Title</th>
-            <th>Price</th>
-            {/* is current user a seller? then buyer otherwise seller */}
-            <th>{currentUser?.isSeller ? "Buyer" : "Seller"}</th>
-            <th>Contact</th>
-          </tr>
-          <tr>
-            <td>
-              <img className="img" src="https://images.pexels.com/photos/11295165/pexels-photo-11295165.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" />
-            </td>
-            <td>Gig1</td>
-            <td>100</td>
-            <td>150</td>
-            <td>
-              <img className="delete" src="/img/message.png" alt="" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <img className="img" src="https://images.pexels.com/photos/11295165/pexels-photo-11295165.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" />
-            </td>
-            <td>Gig1</td>
-            <td>100</td>
-            <td>150</td>
-            <td>
-              <img className="delete" src="/img/message.png" alt="" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <img className="img" src="https://images.pexels.com/photos/11295165/pexels-photo-11295165.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" />
-            </td>
-            <td>Gig1</td>
-            <td>100</td>
-            <td>150</td>
-            <td>
-              <img className="delete" src="/img/message.png" alt="" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <img className="img" src="https://images.pexels.com/photos/11295165/pexels-photo-11295165.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" />
-            </td>
-            <td>Gig1</td>
-            <td>100</td>
-            <td>150</td>
-            <td>
-              <img className="delete" src="/img/message.png" alt="" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <img className="img" src="https://images.pexels.com/photos/11295165/pexels-photo-11295165.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" />
-            </td>
-            <td>Gig1</td>
-            <td>100</td>
-            <td>150</td>
-            <td>
-              <img className="delete" src="/img/message.png" alt="" />
-            </td>
-          </tr>
+        <table>
+            <tr>
+              <th>Image</th>
+              <th>Title</th>
+              <th>Price</th>
+              {/* is current user a seller? then buyer otherwise seller */}
+              {/* <th>{currentUser?.isSeller ? "Buyer" : "Seller"}</th> */}
+              <th>Contact</th>
+            </tr>
+           { data.map((order)=>(
+           <tr key={order.id}>
+              <td>
+                <img className="img" src={order.img} alt="" />
+              </td>
+              <td>{order.title}</td>
+              <td>{order.price}</td>
+              <td>
+                <img onClick={()=>handleContact(order)} className="delete" src="/img/message.png" alt="" />
+              </td>
+            </tr>))}
         </table>
-      </div>
+      </div>)}
     </div>
   )
 }
