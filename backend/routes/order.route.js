bfd250d34413e208d5019ec4ba7712386a593b67
AworThales <thals4jesus@gmail.com>
2023-09-15 16:27:08 +0100

    payment intent and comfirm payment added

--- backend/routes/order.route.js
+++ backend/routes/order.route.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { createOrder, getOrders } from "../controllers/order.controller.js";
+import { confirmOrder, getOrders, paymentIntent } from "../controllers/order.controller.js";
 import {verifyToken} from "../middleware/verify.js"
 
 const router = express.Router();
 
-router.post("/:gigId", verifyToken, createOrder);
+// router.post("/:gigId", verifyToken, createOrder);
 router.get("/", verifyToken, getOrders);
+router.post("/create-payment-intent/:id", verifyToken, paymentIntent);
+router.put("/", verifyToken, confirmOrder);
 
 export default router;
