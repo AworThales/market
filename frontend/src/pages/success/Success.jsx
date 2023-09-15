4f3c965585218068bafbe16fea113f0a39ec4e27
AworThales <thals4jesus@gmail.com>
2023-09-15 16:21:44 +0100

    Success page updated

--- frontend/src/pages/success/Success.jsx
+++ frontend/src/pages/success/Success.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { URLSearchParams } from 'url';
+import newRequest from '../../utils/newRequest';
 
 export const Success = () => {
+    const navigate = useNavigate();
+    const {search} = useLocation();
+    const params = new URLSearchParams(search);
+    const payment_intent = params.get("payment_intent");
+
+    //when we open this page it going to run this useEffect immediately
+    useEffect(()=>{
+        const makeRequest = async () =>{
+            try {
+                await newRequest.put("orders", {payment_intent});
+                setTimeout(()=>{
+                    navigate("/orders");
+                }, 6000);
+            } catch (err) {
+                console.log(err);
+            }
+        };
+        makeRequest();
+    }, []);
+
   return (
-    <div></div>
+    <div>
+        Payment successful, You are being redirected to the orders page.
+        please do not close this page.
+    </div>
   )
 }
